cb3a953b3767867a0e46e6648d1d6b9782176ea0
Gaurav Juneja <junejagaurav0786@gmail.com>
2024-04-18 17:08:01 +0530

    doublet

--- PRACTICE QUESTION ON BASICS OF C++/VECTORS/26-March/doublet.cpp
+++ PRACTICE QUESTION ON BASICS OF C++/VECTORS/26-March/doublet.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
+    int arr[6]={2,4,3,5,6,7};
+    int z;
+
+    cout<<"Enter the value for the sum: "<<endl;
+    cin>>z;
+    
+
+    for(int x=0; x<6; x++){
+        for (int y=x+1; y<6; y++){
+            if(arr[x]+arr[y]==z){
+                cout<<"Elements are "<<arr[x]<<" "<<arr[y]<<endl;
+
+            }
+    
+
+    }
+    }
+}
